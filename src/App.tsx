1ed26e5cf90fc6407c25d02959e5b372ae2661e0
Leonardo Morlacca <leo.morlacca@gmail.com>
2023-05-28 00:46:08 -0300

    Fetch games

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react"
 import Navbar from "./components/Navbar"
+import GameGrid from "./components/GameGrid"
 
 function App() {
   return (
@@ -12,12 +13,10 @@ function App() {
         <Navbar />
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside" bg="coral">
-          Aside
-        </GridItem>
+        <GridItem area="aside">Aside</GridItem>
       </Show>
-      <GridItem area="main" bg="blue">
-        Main
+      <GridItem area="main">
+        <GameGrid />
       </GridItem>
     </Grid>
   )
---
+++ src/components/GameGrid.tsx
@@ -0,0 +1,18 @@
+import useGames from "../hooks/useGames"
+
+const GameGrid = () => {
+  const { games, error } = useGames()
+
+  return (
+    <>
+      {error && <p>{error}</p>}
+      <ul>
+        {games.map(game => (
+          <li key={game.id}>{game.name}</li>
+        ))}
+      </ul>
+    </>
+  )
+}
+
+export default GameGrid
