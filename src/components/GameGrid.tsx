9fc286920e86c75a59c44b783702e1d4b335e63d
Leonardo Morlacca <leo.morlacca@gmail.com>
2023-05-28 23:11:14 -0300

    Displaying the genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -12,7 +12,7 @@ const GameGrid = () => {
     <>
       {error && <p>{error}</p>}
       <SimpleGrid
-        spacing={10}
+        spacing={3}
         padding="10px"
         columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}>
         {isLoading &&
