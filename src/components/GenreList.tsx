4cd2a72198b39cc9f7e1adf18aa16f3a6a4415b7
Leonardo Morlacca <leo.morlacca@gmail.com>
2023-05-28 23:51:31 -0300

    Highlight the selected Genre, and added filter by genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,8 +1,20 @@
-import { HStack, List, ListItem, Image, Text, Spinner } from "@chakra-ui/react"
-import useGenres from "../hooks/useGenres"
+import {
+  HStack,
+  List,
+  ListItem,
+  Image,
+  Button,
+  Spinner,
+} from "@chakra-ui/react"
+import useGenres, { Genre } from "../hooks/useGenres"
 import getCroppedImageUrl from "../services/image-url"
 
-const GenreList = () => {
+interface Props {
+  onSelectGenre: (genre: Genre) => void
+  selectedGenre: Genre | null
+}
+
+const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
   const { data, isLoading, error } = useGenres()
 
   if (error) return null
@@ -18,7 +30,13 @@ const GenreList = () => {
               borderRadius={8}
               src={getCroppedImageUrl(genre.image_background)}
             />
-            <Text fontSize="lg">{genre.name}</Text>
+            <Button
+              onClick={() => onSelectGenre(genre)}
+              variant="link"
+              fontWeight={selectedGenre?.id === genre.id ? "bold" : "normal"}
+              fontSize="lg">
+              {genre.name}
+            </Button>
           </HStack>
         </ListItem>
       ))}
