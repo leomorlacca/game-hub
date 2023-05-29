6c050d8185a3380e4cd8b9f2b6440b4fa174d2a7
Leonardo Morlacca <leo.morlacca@gmail.com>
2023-05-28 22:15:03 -0300

    Refactor: deleted duplicated code

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -2,6 +2,7 @@ import { SimpleGrid } from "@chakra-ui/react"
 import useGames from "../hooks/useGames"
 import GameCard from "./GameCard"
 import GameCardSkeleton from "./GameCardSkeleton"
+import GameCardContainer from "./GameCardContainer"
 
 const GameGrid = () => {
   const { games, error, isLoading } = useGames()
@@ -14,9 +15,16 @@ const GameGrid = () => {
         spacing={10}
         padding="10px"
         columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}>
-        {isLoading && skeletons.map(e => <GameCardSkeleton key={e} />)}
+        {isLoading &&
+          skeletons.map(e => (
+            <GameCardContainer key={e}>
+              <GameCardSkeleton />
+            </GameCardContainer>
+          ))}
         {games.map(game => (
-          <GameCard key={game.id} game={game} />
+          <GameCardContainer key={game.id}>
+            <GameCard game={game} />
+          </GameCardContainer>
         ))}
       </SimpleGrid>
     </>
